017130115332cb6f3cd5fc243134c819669edabc
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-07-10 21:22:57 +0545

    Initial commit : setup Spring boot project structure

---
+++ src/main/java/com/rbums/rbums/RbumsApplication.java
@@ -0,0 +1,13 @@
+package com.rbums.rbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbums/rbums/RbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbums.rbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
