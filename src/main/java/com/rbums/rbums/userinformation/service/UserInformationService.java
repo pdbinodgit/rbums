a07f2b8d9a008393c7743aa43f74700b90ae2f4d
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-07-10 21:57:02 +0545

    Add UserInformationService interface  with related methods

---
+++ src/main/java/com/rbums/rbums/userinformation/service/UserInformationService.java
@@ -0,0 +1,20 @@
+package com.rbums.rbums.userinformation.service;
+
+import com.rbums.rbums.userinformation.dto.UserInformationDto;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface UserInformationService {
+
+     void  saveUserInformation(UserInformationDto userInformationDto);
+
+     List<UserInformationDto> getAllUser();
+
+     UserInformationDto getByUserId();
+
+     List<UserInformationDto> searchByName(String keyboard);
+
+     void updateUserInformation(UserInformationDto userInformationDto);
+
+}
