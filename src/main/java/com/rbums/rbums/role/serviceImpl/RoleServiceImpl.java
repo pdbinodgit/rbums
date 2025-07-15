5aff4ba3ba58c5f9d99762d81ee31a4f27347bc4
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-07-15 20:48:03 +0545

    adding custom exception in role class

--- src/main/java/com/rbums/rbums/role/serviceImpl/RoleServiceImpl.java
+++ src/main/java/com/rbums/rbums/role/serviceImpl/RoleServiceImpl.java
@@ -1,15 +1,19 @@
 package com.rbums.rbums.role.serviceImpl;
 
+import com.rbums.rbums.customException.RbumsCustomException;
 import com.rbums.rbums.mapperinterface.RoleMapper;
 import com.rbums.rbums.role.dto.RoleDto;
 import com.rbums.rbums.role.model.Role;
 import com.rbums.rbums.role.repository.RoleRepository;
 import com.rbums.rbums.role.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RoleServiceImpl implements RoleService {
 
@@ -20,7 +24,12 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public RoleDto saveRole(RoleDto roleDto) {
+
         Role role=roleMapper.dtoToEntity(roleDto);
+        Optional<Role> roleOptional=roleRepository.findByRoleName(role.getRoleName());
+        if (roleOptional.isPresent()){
+            throw new RbumsCustomException("Role name already present", HttpStatus.BAD_REQUEST,400);
+        }
         roleRepository.save(role);
         RoleDto dto=roleMapper.entityToDto(role);
         return dto;
