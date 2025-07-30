ee6935078c935715e7cc90926d81fec0a8b50c6c
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-07-30 22:22:52 +0545

    adding attendence repository class

---
+++ src/main/java/com/rbums/rbums/attendance/repository/AttendanceRepository.java
@@ -0,0 +1,10 @@
+package com.rbums.rbums.attendance.repository;
+
+import com.rbums.rbums.attendance.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
+
+}
